1b5f1df1070e6c1707d11106a2d1abc5c9909669
samir benzada <samirbenzada@gmail.com>
2019-11-19 14:28:39 -0500

    update

--- src/js/components/Navbar/Navbar.js
+++ src/js/components/Navbar/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Navbar.module.css';
-import plane from '../../../img/plane.png';
+import plane from '../../../img/plane.gif';
 
 const AircraftTrainNavBar = () => {
     
