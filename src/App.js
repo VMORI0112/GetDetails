39d812034fcc99a6f7f193d6b68f0158addee578
samir benzada <samirbenzada@gmail.com>
2019-11-19 13:51:20 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { Suspense, lazy } from 'react';
+import React, { Suspense, lazy, useState, useMemo, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { UserContext } from './UserContext';
 import './App.css';
 import Footer from './js/components/Footer/Footer';
 import Navbar from './js/components/Navbar/Navbar';
@@ -12,19 +13,34 @@ function App() {
   let tokenAuth = localStorage.getItem('token');
   // let userId = localStorage.getItem('userID');
   // let userEmail = localStorage.getItem('email');
+  const [windowHeight, setWindowHeight] = useState(window.innerHeight);
+
+  useEffect(() => {
+    const handleResize = () => setWindowHeight(window.innerHeight);
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  },[])
+
+  const providerValue = useMemo(() => ({ windowHeight }), [windowHeight]);
 
   return (
     <Router>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Navbar/>
-        <Switch>
-          {!tokenAuth ? <Route exact path="/" component={Home} /> : <Route exact path="/" component={LoggedHome}/> }
-          {/* <Route path="/login" component={Login} />
-          <Route path="/register" component={Register} />
-          <Route render={() => <NotFound/>} /> */}
-        </Switch>
-        <Footer/>
-      </Suspense>
+      <UserContext.Provider value={providerValue}>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Navbar/>
+            <section style={{minHeight: windowHeight}}>
+              <Switch>
+                {!tokenAuth ? <Route exact path="/" component={Home} /> : <Route exact path="/" component={LoggedHome}/> }
+                {/* <Route path="/login" component={Login} />
+                <Route path="/register" component={Register} />
+                <Route render={() => <NotFound/>} /> */}
+              </Switch>
+            </section>
+          <Footer/>
+        </Suspense>
+      </UserContext.Provider>
     </Router> 
   );
 }
