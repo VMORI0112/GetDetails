1dfe8d7685e8dda311357c773b52f66f05196e67
samir benzada <samirbenzada@gmail.com>
2019-11-20 15:02:54 -0500

    update

--- src/js/views/Home/Home.js
+++ src/js/views/Home/Home.js
@@ -64,6 +64,9 @@ const Home = () => {
                     </div>
                 </div>
             </div>
+            <div className={styles.services}>
+                <h2>Services</h2>
+            </div>
         </section>
     );
 };
