4943ebb012383010fc8128bf1d3a557cd1f4bfdb
Samir Benzada <samirbenzada@gmail.com>
2019-11-23 13:25:00 -0500

    update

--- src/js/views/LoggedHome/LoggedHome.js
+++ src/js/views/LoggedHome/LoggedHome.js
@@ -4,8 +4,8 @@ import styles from './LoggedHome.module.css';
 const LoggedHome = () => {
     return (
         <section className={styles.section}>
-            <h1>Aicraft Training Records</h1>
-            
+            <h1 className="text-center">Aicraft Training Records</h1>
+
         </section>
     );
 };
