af9dd7206e21b2edc50c6a7d96531b987d85db1b
samir benzada <samirbenzada@gmail.com>
2019-12-18 14:18:32 -0500

    update

---
+++ src/js/views/Chat/Chat.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Chat.module.css';
+
+const Chat = () => {
+    return (
+        <section className={styles.section}>
+            chat page
+        </section>
+    );
+};
+
+export default Chat;
