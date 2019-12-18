7b3c27a2504c1a09eb7e396a860c237a2d4e38f7
samir benzada <samirbenzada@gmail.com>
2019-12-18 14:48:40 -0500

    -m

--- src/js/views/Chat/Chat.js
+++ src/js/views/Chat/Chat.js
@@ -9,28 +9,33 @@ let avatar = localStorage.getItem('avatar');
 const Chat = () => {
     return (
         <section className={styles.section}>
-            <div className={styles.container}>
-                <img src={process.env.PUBLIC_URL + '/img/avatar/' + avatar} alt="Avatar" width="30px" />
-                <p className={styles.leftP}>Hello. How are you today?</p>
-                <span className={styles.timeRight}>11:00</span>
-            </div>
+            <div className="container">
+                <div className="alert alert-primary">
+                    <div> {firstname} {lastname} </div>
+                </div>
+                <div className={styles.container}>
+                    <img src={process.env.PUBLIC_URL + '/img/avatar/' + avatar} alt="Avatar" width="30px" />
+                    <p className={styles.leftP}>Hello. How are you today?</p>
+                    <span className={styles.timeRight}>11:00</span>
+                </div>
 
-            <div className={[styles.container, styles.darker].join(' ')}>
-                <img className={styles.right} src={process.env.PUBLIC_URL + '/img/avatar/' + avatar} alt="Avatar" width="30px" />
-                <p className={styles.rightP}>Hey! I'm fine. Thanks for asking!</p>
-                <span className={styles.timeLeft}>11:01</span>
-            </div>
+                <div className={[styles.container, styles.darker].join(' ')}>
+                    <img className={styles.right} src={process.env.PUBLIC_URL + '/img/avatar/' + avatar} alt="Avatar" width="30px" />
+                    <p className={styles.rightP}>Hey! I'm fine. Thanks for asking!</p>
+                    <span className={styles.timeLeft}>11:01</span>
+                </div>
 
-            <div className={styles.container}>
-                <img src={process.env.PUBLIC_URL + '/img/avatar/' + avatar} alt="Avatar" width="30px" />
-                <p className={styles.leftP}>Sweet! So, what do you wanna do today?</p>
-                <span className={styles.timeRight}>11:02</span>
-            </div>
+                <div className={styles.container}>
+                    <img src={process.env.PUBLIC_URL + '/img/avatar/' + avatar} alt="Avatar" width="30px" />
+                    <p className={styles.leftP}>Sweet! So, what do you wanna do today?</p>
+                    <span className={styles.timeRight}>11:02</span>
+                </div>
 
-            <div className={[styles.container, styles.darker].join(' ')}>
-                <img className={styles.right} src={process.env.PUBLIC_URL + '/img/avatar/' + avatar} alt="Avatar" width="30px" />
-                <p className={styles.rightP}>Nah, I dunno. Play soccer.. or learn more coding perhaps?</p>
-                <span className={styles.timeLeft}>11:05</span>
+                <div className={[styles.container, styles.darker].join(' ')}>
+                    <img className={styles.right} src={process.env.PUBLIC_URL + '/img/avatar/' + avatar} alt="Avatar" width="30px" />
+                    <p className={styles.rightP}>Nah, I dunno. Play soccer.. or learn more coding perhaps?</p>
+                    <span className={styles.timeLeft}>11:05</span>
+                </div>
             </div>
         </section>
     );
